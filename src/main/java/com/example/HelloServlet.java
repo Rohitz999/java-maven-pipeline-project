8f768b64235f4a90f8307c9505fd844d0d91d2a0
root <rohitvishwakarma8082@gmail.com>
2026-02-02 18:06:44 +0000

    intial commit

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +14,19 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         response.setContentType("text/html;charset=UTF-8");
-        response.getWriter().println("<h1>Hello From, World!</h1>");
+
+        response.getWriter().println(
+            "<html>" +
+            "<head><title>Hello Servlet</title></head>" +
+            "<body>" +
+            "<h1>Hello from Jenkins Build 🚀</h1>" +
+            "<p>Application deployed successfully.</p>" +
+            "<p>Build time: " + LocalDateTime.now() + "</p>" +
+            "</body>" +
+            "</html>"
+        );
     }
 }
 
